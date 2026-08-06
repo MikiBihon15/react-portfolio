c1d56a50372e2b4f3a9dd41fd758ad2268941189
MikiBihon15 <mikhaildustin@gmail.com>
2026-08-06 13:48:17 +0800

    projects-update

---
+++ src/components/projects.jsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <section style={{ padding: '20px' }}>
+      <h2>Projects</h2>
+      <ul>
+        <li>Some of my projects</li>
+      </ul>
+    </section>
+  );
+}
