051a99f240f7aaa2552feaf17208f000df547b27
MikiBihon15 <mikhaildustin@gmail.com>
2026-08-06 13:46:37 +0800

    contacts-update

---
+++ src/components/contacts.jsx
@@ -0,0 +1,8 @@
+export default function Contacts() {
+  return (
+    <section style={{ padding: '20px' }}>
+      <h2>Contact Me</h2>
+      <p>Email: mikhaildustin@gmail.com</p>
+    </section>
+  );
+}
