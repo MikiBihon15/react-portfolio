e1e0e1c1ec42ae2c6a87546b4455f0388daa9a3f
MikiBihon15 <mikhaildustin@gmail.com>
2026-08-06 13:38:48 +0800

    About-updated contents

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <section style={{ padding: '20px' }}>
       <h2>About Me</h2>
-      <p>Hello! I am a student learning React and building my first single-page application.</p>
+      <p>Hello! I am Mikhail Dustin Caasi Elpedes, welcome to a web development project of mine with the use of React.</p>
     </section>
   );
 }
