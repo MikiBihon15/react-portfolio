e9bb90ab8004e309512ecd0049e9a85bd9ba70a5
MikiBihon15 <mikhaildustin@gmail.com>
2026-08-06 18:03:32 +0800

    reverted changes

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -2,7 +2,7 @@ export default function Contacts() {
   return (
     <section style={{ padding: '20px' }}>
       <h2>Contact Me</h2>
-      <p>Email: Testmikhaildustin@gmail.com</p>
+      <p>Email: mikhaildustin@gmail.com</p>
     </section>
   );
 }
