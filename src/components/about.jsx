fb7de29452726e7a40971cc6760f3e28991f4d40
MikiBihon15 <mikhaildustin@gmail.com>
2026-08-06 13:35:12 +0800

    components-about

---
+++ src/components/about.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section style={{ padding: '20px' }}>
+      <h2>About Me</h2>
+      <p>Hello! I am a student learning React and building my first single-page application.</p>
+    </section>
+  );
+}
